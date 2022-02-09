f016cb172a26e0308d1b81f5d948eff940392ef8
kimjngyun <sanbada79@naver.com>
2022-02-09 16:31:03 +0900

    feat: Add axios default config

---
+++ src/infrastructure/api/baseAPI.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosRequestConfig } from 'axios';
+const API_URL = 'https://dev-api.gist-petition.com/v1/';
+const config: AxiosRequestConfig = {
+  baseURL: API_URL,
+  validateStatus: (status: number) => status < 500,
+  timeout: 10000,
+  withCredentials: true,
+};
+
+const API = axios.create(config);
+
+export default API;
