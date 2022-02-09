c4f5973e5955263862c2b0e79872b2346d4ea4da
kimjngyun <sanbada79@naver.com>
2022-02-09 17:11:14 +0900

    feat: redux store 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import React from 'react';
 import { GlobalStyle } from './styles/globalStyle';
 import MainRouter from '@routes/MainRouter';
-
+import { Provider } from 'react-redux';
+import { store } from '@stores/store';
 const App = (): JSX.Element => {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <MainRouter />
-    </>
+    </Provider>
   );
 };
 export default App;
