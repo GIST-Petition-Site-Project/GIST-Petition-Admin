bd44958f549afc606ed9e0b161a578a1fddc091e
kimjngyun <sanbada79@naver.com>
2022-02-13 17:08:48 +0900

    feat: toast Hooks 추가

---
+++ src/application/hooks/useToast.ts
@@ -0,0 +1,18 @@
+import { addToast, popToast } from '@stores/toastSlice';
+import { useAppDispatch, useAppSelect } from '@hooks/useStore';
+
+export const useToast = () => {
+  const dispatch = useAppDispatch();
+  const getRandomID = () => String(new Date().getTime());
+
+  const fireToast = ({ message, type, duration = 2000 }: Toast) => {
+    const id = getRandomID();
+    dispatch(addToast({ message, type, duration, id }));
+    console.log('Toast added');
+    setTimeout(() => dispatch(popToast({ id })), duration + 600);
+  };
+
+  return fireToast;
+};
+
+export const useToasts = () => useAppSelect((state) => state.toast);
