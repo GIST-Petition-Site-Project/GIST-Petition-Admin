2f837cea67857fc42f0d0e90e858ea552bf66f67
kimjngyun <sanbada79@naver.com>
2022-02-13 17:09:33 +0900

    feat: 컴포넌트 최상단에 Toast 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import MainRouter from '@routes/MainRouter';
 import { Provider } from 'react-redux';
 import { persistor, store } from '@stores/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import Toast from '@components/Toast/Toast';
 const App = (): JSX.Element => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <GlobalStyle />
+        <Toast />
         <MainRouter />
       </PersistGate>
     </Provider>
