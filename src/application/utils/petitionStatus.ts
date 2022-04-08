9280f2f7fcbb2aced9dc47e6c0bfd7176e144100
kimjngyun <sanbada79@naver.com>
2022-04-08 14:32:13 +0900

    refactor: 파라미터 변경

--- src/application/utils/petitionStatus.ts
+++ src/application/utils/petitionStatus.ts
@@ -1,19 +1,14 @@
-const checkPetitionStatus = (
-  released: boolean,
-  agreeCount: number,
-  answered: boolean,
-  rejected: boolean,
-  expired: boolean,
-): PetitionStatus => {
+const checkPetitionStatus = (petition?: Petition): PetitionStatus => {
+  if (!petition) return '청원 진행중';
   /**
    * @description 답변을 요청하기 위한 최소 동의 수
    */
   const REQUIRED_AGREEMENT_COUNTS = 50;
-  if (!released) return '승인 대기중';
-  if (rejected) return '승인 반려됨';
-  if (answered) return '답변 완료됨';
-  if (expired) return '청원 만료됨';
-  return agreeCount > REQUIRED_AGREEMENT_COUNTS ? '답변 대기중' : '청원 진행중';
+  if (!petition.released) return '승인 대기중';
+  if (petition.rejected) return '승인 반려됨';
+  if (petition.answered) return '답변 완료됨';
+  if (petition.expired) return '청원 만료됨';
+  return petition.agreeCount > REQUIRED_AGREEMENT_COUNTS ? '답변 대기중' : '청원 진행중';
 };
 
 export default checkPetitionStatus;
